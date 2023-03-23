429fcd168c8b0124280a9b0d553ee445cbcfdcc7
menneck <zeeeee.peeeeetryyy@gmail.com>
2023-03-23 18:54:12 -0300

    Create PersonService and basic methods, getById and create

--- src/main/java/com/minhasacoes/Service/PersonService.java
+++ src/main/java/com/minhasacoes/Service/PersonService.java
@@ -1,7 +1,36 @@
 package com.minhasacoes.Service;
 
+import com.minhasacoes.DTO.PersonDTO;
+import com.minhasacoes.Model.Person;
+import com.minhasacoes.Repository.PersonRepository;
+import com.minhasacoes.Service.Exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class PersonService {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    public Person findById(Integer id) {
+        Optional<Person> optionalPerson = personRepository.findById(id);
+        return optionalPerson.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    @Transactional
+    public void create(PersonDTO personDTO) {
+        personRepository.save(createPerson(personDTO));
+    }
+
+    private Person createPerson(PersonDTO personDTO) {
+        Person person = new Person();
+        person.setName(personDTO.getName());
+
+        return person;
+    }
+
 }
