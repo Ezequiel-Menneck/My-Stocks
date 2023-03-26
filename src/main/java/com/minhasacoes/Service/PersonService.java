dc01c4415c8e548f2ac73d0b9ed7fb907d34016f
menneck <zeeeee.peeeeetryyy@gmail.com>
2023-03-25 23:52:01 -0300

    Created method to add stocks to user wallet

--- src/main/java/com/minhasacoes/Service/PersonService.java
+++ src/main/java/com/minhasacoes/Service/PersonService.java
@@ -1,7 +1,10 @@
 package com.minhasacoes.Service;
 
 import com.minhasacoes.DTO.PersonDTO;
+import com.minhasacoes.DTO.StocksDTO;
+import com.minhasacoes.Model.MarketData;
 import com.minhasacoes.Model.Person;
+import com.minhasacoes.Model.Stocks;
 import com.minhasacoes.Repository.PersonRepository;
 import com.minhasacoes.Service.Exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +17,10 @@ import java.util.Optional;
 public class PersonService {
 
     @Autowired
-    PersonRepository personRepository;
+    private PersonRepository personRepository;
+
+    @Autowired
+    private StocksService stocksService;
 
     public Person findById(Integer id) {
         Optional<Person> optionalPerson = personRepository.findById(id);
@@ -31,6 +37,31 @@ public class PersonService {
         person.setName(personDTO.getName());
 
         return person;
+    }
+
+    @Transactional
+    public void addStocksToPerson(Integer id, StocksDTO stocksDTO) {
+        Person person = findById(id);
+        Stocks stocks = new Stocks();
+
+        MarketData marketData = stocksService.getInfoBySymbol(stocksDTO.getSymbol());
+
+        for (MarketData.Result mkd : marketData.getResults()) {
+            stocks.setSymbol(mkd.getSymbol());
+            stocks.setLongName(mkd.getLongName());
+            stocks.setCurrency(mkd.getCurrency());
+
+            if (stocksDTO.getRegularMarketPrice() == null) {
+                stocks.setRegularMarketPrice(mkd.getRegularMarketPrice());
+            } else {
+                stocks.setRegularMarketPrice(stocksDTO.getRegularMarketPrice());
+            }
+
+            stocks.setQuantity(stocksDTO.getQuantity());
+        }
+        person.getWallet().add(stocks);
+
+        personRepository.save(person);
     }
 
 }
