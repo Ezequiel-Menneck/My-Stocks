dc09a5690e693a7b0829be625489fbdcb8ba0077
menneck <zeeeee.peeeeetryyy@gmail.com>
2023-03-27 18:48:16 -0300

    Create stocks controller and method to getAllData by Symbol of stock

--- src/main/java/com/minhasacoes/Controller/StocksController.java
+++ src/main/java/com/minhasacoes/Controller/StocksController.java
@@ -1,5 +1,12 @@
 package com.minhasacoes.Controller;
 
+import com.minhasacoes.Model.MarketData;
+import com.minhasacoes.Service.StocksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/stocks")
 public class StocksController {
 
+    @Autowired
+    private StocksService stocksService;
 
+    @GetMapping("/{symbol}")
+    public ResponseEntity<MarketData> getInfoBySymbol(@PathVariable String symbol) {
+        return new ResponseEntity<>(stocksService.getInfoBySymbol(symbol), HttpStatus.OK);
+    }
 
 }
